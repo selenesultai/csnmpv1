ff1bff7598f5919c70ab11de786f3b3da0d94635
selenesultai <anne@thedoghouse.cc>
2024-08-23 17:16:49 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,6 @@ pub mod message;
 pub mod oid;
 
 
-pub use crate::client::{Snmp2cClient, SnmpClientError};
+pub use crate::client::{Snmp1Client, SnmpClientError};
 pub use crate::oid::{ObjectIdentifier, ObjectIdentifierConversionError};
 pub use crate::message::ObjectValue;
